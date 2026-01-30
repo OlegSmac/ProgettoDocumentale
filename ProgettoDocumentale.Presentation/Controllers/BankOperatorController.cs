cfb3cd435e63dce6110343b41811f3a698d45e3a
OlegSmac <oleansmacinih@gmail.com>
2026-01-30 08:57:14 +0200

    Bank operator initialization

---
+++ ProgettoDocumentale.Presentation/Controllers/BankOperatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using MediatR;
+
+namespace ProgettoDocumentale.Presentation.Controllers
+{
+    [Authorize(Roles = "BankOperator")]
+    public class BankOperatorController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BankOperatorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public ActionResult Index(CancellationToken cancellationToken)
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot send request via Mediatr" + ex);
+            }
+        }
+    }
+}
