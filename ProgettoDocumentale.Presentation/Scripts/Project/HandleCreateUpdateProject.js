e4077c544904e02f273204c5384ac77de299d986
OlegSmac <oleansmacinih@gmail.com>
2026-02-03 11:41:28 +0200

    Selectpicker refresh

--- ProgettoDocumentale.Presentation/Scripts/Project/HandleCreateUpdateProject.js
+++ ProgettoDocumentale.Presentation/Scripts/Project/HandleCreateUpdateProject.js
@@ -2,6 +2,7 @@ function handleCreateUpdateProject(html) {
     $('#appModalContent').empty().html(html);
 
     var success = $('#appModalContent').find('[data-success]').first().attr('data-success');
+    $('#appModalContent .selectpicker').selectpicker();
 
     if (success === '1') {
         $('#appModal').modal('hide');        
--- ProgettoDocumentale.Presentation/Scripts/User/HandleCreateUpdateUser.js
+++ ProgettoDocumentale.Presentation/Scripts/User/HandleCreateUpdateUser.js
@@ -2,9 +2,10 @@ function handleCreateUpdateUser(html) {
     $('#appModalContent').empty().html(html);
 
     var success = $('#appModalContent').find('[data-success]').first().attr('data-success');
+    $('#appModalContent .selectpicker').selectpicker();
 
     if (success === '1') {
-        $('#appModal').modal('hide'); 
+        $('#appModal').modal('hide');        
 
         usersTable?.ajax.reload(null, false);
     }
